ee7c1512e59218d41723d8b6e97f958bb491d508
pomelo-nwu <czynwu@outlook.com>
2023-08-21 15:53:12 +0800

    feat: update icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import * as components from "./components";
-import * as deploys from "./deploys";
-import services from "./services";
-import * as templates from "./templates";
+import * as components from './components';
+import * as deploys from './deploys';
+import services from './services';
+import * as templates from './templates';
 
-export { components, deploys, services, templates };
+const icons = ['font_3146710_g7j86m2izbv'];
+
+export { components, deploys, icons, services, templates };
