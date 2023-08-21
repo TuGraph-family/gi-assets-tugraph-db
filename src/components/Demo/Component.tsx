ba0fed0bc40c5f6a6570562e232524eea5ef61a8
noneAuth <wb-zqm915773@antgroup.com>
2023-08-21 15:05:44 +0800

    feat： 增加逻辑

--- src/components/Demo/Component.tsx
+++ src/components/Demo/Component.tsx
@@ -1,11 +1,38 @@
-import { useContext } from '@antv/gi-sdk';
-import { Modal } from 'antd';
+import { useContext, utils } from '@antv/gi-sdk';
+import { Modal, message } from 'antd';
 import React, { useState } from 'react';
+import { useImmer } from 'use-immer';
 import demoData from './data/demo1.json';
 import './index.less';
+import { getTransformByTemplate } from '../StyleSetting/utils';
 
-const Demo = () => {
-  const { transform, updateContext, services } = useContext();
+export interface ILanguageQueryProps {
+  height?: string;
+  languageServiceId: string;
+}
+
+const Demo: React.FC<ILanguageQueryProps> = ({ languageServiceId }) => {
+  const { transform, updateContext, services, schemaData, graph } = useContext();
+  const customStyleConfig = localStorage.getItem('CUSTOM_STYLE_CONFIG')
+    ? JSON.parse(localStorage.getItem('CUSTOM_STYLE_CONFIG') as string)
+    : {};
+  console.log(services, languageServiceId, 'services');
+
+  const [state, setState] = useImmer<{
+    languageType: string;
+    editorValue: string;
+    btnLoading: boolean;
+    hasClear: boolean;
+  }>({
+    languageType: 'Cypher',
+    editorValue: 'match p=(n)-[*..1]-(m)  RETURN p LIMIT  50',
+    btnLoading: false,
+    hasClear: false,
+  });
+  const { languageType, editorValue, btnLoading } = state;
+  const languageService: any = utils.getService(services, languageServiceId);
+  const graphName = 'default';
+
   const [config, setConfig] = useState({
     visible: true,
     index: 0,
@@ -19,20 +46,63 @@ const Demo = () => {
     <div className="demo">
       <Modal
         visible={config.visible}
+        // confirmLoading={btnLoading}
         width={772}
         cancelText="取消"
         okText="确定"
         onCancel={() => {
           set({ visible: false });
         }}
-        onOk={() => {
+        onOk={async () => {
           const data = transform(demoData);
-          updateContext(draft => {
-            draft.data = data;
-            draft.source = data;
-            draft.isLoading = false;
-            set({ visible: false });
+          const result = await languageService({
+            script: editorValue,
+            graphName,
           });
+          if (!result.success) {
+            // 执行查询失败
+            message.error(`执行查询失败: ${result.errorMessage}`);
+            return;
+          }
+          const { formatData } = result.data;
+          // 处理 formData，添加 data 字段
+          formatData.nodes.forEach(d => {
+            d.data = d.properties;
+          });
+
+          formatData.edges.forEach(d => {
+            d.data = d.properties;
+          });
+          const transformData = getTransformByTemplate(customStyleConfig, schemaData);
+          console.log(result, 'services');
+          if (state.hasClear) {
+            // 清空数据
+            updateContext(draft => {
+              if (transformData) {
+                draft.transform = transformData;
+              }
+
+              const res = transformData(formatData);
+              // @ts-ignore
+              draft.data = res;
+              // @ts-ignore
+              draft.source = res;
+            });
+          } else {
+            // 在画布上叠加数据
+            const originData: any = graph.save();
+            const newData = {
+              nodes: [...originData.nodes, ...formatData.nodes],
+              edges: [...originData.edges, ...formatData.edges],
+            };
+            updateContext(draft => {
+              const res = transformData(newData);
+              // @ts-ignore
+              draft.data = res;
+              // @ts-ignore
+              draft.source = res;
+            });
+          }
         }}
         title={
           <div className="title">
