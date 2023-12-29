d736ef3a2edca450d0cc78d8afbd6858dabb20d0
noneAuth <wb-zqm915773@antgroup.com>
2023-12-29 11:13:24 +0800

    feat: container flex auto fully

--- src/components/LayoutContent/TwoInput.tsx
+++ src/components/LayoutContent/TwoInput.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Input, InputNumber, Form } from 'antd';
 
+const inputStyle: any = {
+  width: 104, 
+  borderRadius: '4px'
+};
+
 const TwoInput = () => {
   return (
-    <Input.Group>
+    <Input.Group style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    }}>
       <Form.Item name="x" extra="x 坐标" style={{ display: 'inline-block', marginBottom: 0 }}>
-        <InputNumber size="small" style={{ width: 104 }} />
+        <InputNumber size="small" style={inputStyle} />
       </Form.Item>
       <Form.Item
         name="y"
         extra="y 坐标"
         style={{
           display: 'inline-block',
-
           margin: '0 16px',
         }}
       >
-        <InputNumber size="small" style={{ width: 104 }} />
+        <InputNumber size="small" style={inputStyle} />
       </Form.Item>
     </Input.Group>
   );
